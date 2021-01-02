d34dfe2ebe2c7b6b82c8c00b131f8ac1ea981c46
Yesika <yesibcastro@gmail.com>
2021-01-02 15:04:40 -0500

    Primer commit

---
+++ config/config.js
@@ -0,0 +1,5 @@
+//==============================================
+//Configurando puerto de desarrollo o produccion
+//==============================================
+
+process.env.PORT = process.env.PORT || 3000;
---
+++ server/server.js
@@ -0,0 +1,50 @@
+require('../config/config');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+//parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+//parse application/json
+app.use(bodyParser.json())
+
+app.get('/usuario', function(req, res) {
+    res.json('Get usuarios')
+});
+
+app.post('/usuario', function(req, res) {
+
+    let body = req.body;
+
+    if (body.nombre === undefined) {
+
+        res.status(400).json({
+            ok: false,
+            mensaje: 'El nombre es necesario'
+        });
+
+    } else {
+        res.json({
+            datos: body
+        });
+    }
+});
+
+app.put('/usuario/:id', function(req, res) {
+
+    let id = req.params.id;
+
+    res.json({
+        id
+    });
+
+});
+
+app.delete('/usuario', function(req, res) {
+    res.json('Delete usuarios')
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Escuchando el puerto ${process.env.PORT}`);
+})
