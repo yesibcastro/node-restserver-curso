6b415f400356333fc8ee2d342b795c3b6df77434
Yesika <yesibcastro@gmail.com>
2021-01-03 16:47:12 -0500

    RestServer - Usuario

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 require('../config/config');
+
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser');
 
@@ -7,43 +9,14 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }))
 
 //parse application/json
-app.use(bodyParser.json())
-
-app.get('/usuario', function(req, res) {
-    res.json('Get usuarios')
+app.use(bodyParser.json());
+app.use(require('../routes/usuario'));
+mongoose.connect(process.env.URLDB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
-
-app.post('/usuario', function(req, res) {
-
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-
-    } else {
-        res.json({
-            datos: body
-        });
-    }
-});
-
-app.put('/usuario/:id', function(req, res) {
-
-    let id = req.params.id;
-
-    res.json({
-        id
-    });
-
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json('Delete usuarios')
-});
+mongoose.set('useCreateIndex', true);
+mongoose.set('useFindAndModify', false);
 
 app.listen(process.env.PORT, () => {
     console.log(`Escuchando el puerto ${process.env.PORT}`);
