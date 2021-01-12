473310f49f1bee232da43e3810fe0e41b2f57a94
Yesika <yesibcastro@gmail.com>
2021-01-11 22:22:00 -0500

    Se modifica llamado de token

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -66,8 +66,17 @@ app.put('/upload/:tipo/:id', (req, res) => {
     //Cambiar nombre del archivo 
     let nombreArchivo = `${id}-${ new Date().getMilliseconds()}.${extension}`;
 
+    let pathGuardar = `uploads/${tipo}/${nombreArchivo}`;
+    // validar si tenemos los directorios creados
+
+    if (!fs.existsSync(path.resolve(`uploads`))) {
+        crearDirectorio();
+    }
+    if (!fs.existsSync(path.resolve(`uploads/${ tipo }`))) {
+        crearDirectorio(tipo);
+    };
     //Ruta donde se subiran los archivos enviados desde postman
-    archivo.mv(`uploads/${tipo}/${nombreArchivo}`, (err) => {
+    archivo.mv(pathGuardar, (err) => {
 
         if (err) {
             return res.status(500).json({
@@ -168,5 +177,43 @@ function borrarArchivo(nombreImagen, tipo) {
     if (fs.existsSync(pathImagen)) {
         fs.unlinkSync(pathImagen);
     }
-}
+};
+
+function crearDirectorio(tipo) {
+
+    switch (tipo) {
+        case 'usuarios':
+            fs.mkdir(`uploads/${ tipo }`, err => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log('Directory Created');
+                }
+            });
+            break;
+
+        case 'productos':
+            fs.mkdir(`uploads/${ tipo }`, err => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log('Directory Created');
+                }
+            });
+            break;
+
+        default:
+
+            fs.mkdir('uploads', err => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log('Directory Created');
+                }
+            });
+
+            break;
+    }
+
+};
 module.exports = app;
