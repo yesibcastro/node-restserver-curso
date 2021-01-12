5751ff72b3805503112697b0744d0bd9de88a6f8
Yesika <yesibcastro@gmail.com>
2021-01-11 22:36:46 -0500

    se copian archivos

--- server/routes/imagenes.js
+++ server/routes/imagenes.js
@@ -1,17 +1,22 @@
 const express = require('express');
+
 const fs = require('fs');
 const path = require('path');
+
 const { verificaTokenImg } = require('../middlewares/autenticacion');
+
+
 let app = express();
 
 
 app.get('/imagen/:tipo/:img', verificaTokenImg, (req, res) => {
+
+
     let tipo = req.params.tipo;
     let img = req.params.img;
 
-    //cada argumento del resolve son segmentos del path que se quiere construir
-    let pathImagen = path.resolve(__dirname, `../../uploads/${tipo}/${img}`);
-    console.log(pathImagen);
+    let pathImagen = path.resolve(__dirname, `../../uploads/${ tipo }/${ img }`);
+
     if (fs.existsSync(pathImagen)) {
         res.sendFile(pathImagen);
     } else {
@@ -19,7 +24,11 @@ app.get('/imagen/:tipo/:img', verificaTokenImg, (req, res) => {
         res.sendFile(noImagePath);
     }
 
+
+
 });
+
+
 
 
 
--- server/routes/upload.js
+++ server/routes/upload.js
@@ -1,56 +1,49 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
-
 const app = express();
-//Importar entidades
+
 const Usuario = require('../models/usuario');
 const Producto = require('../models/producto');
+
 const fs = require('fs');
-//Constante para construir path al buscar una imagen
 const path = require('path');
 
-//opciones por defecto 
+
+// default options
 app.use(fileUpload());
 
 
-app.put('/upload/:tipo/:id', (req, res) => {
+app.put('/upload/:tipo/:id', function(req, res) {
 
-    //Variable para identificar si la imagen es de usuarios o productos
     let tipo = req.params.tipo;
-    //Variable para identificar el id del usuario o producto a actualizar
     let id = req.params.id;
 
     if (!req.files) {
-        return res.status(400).json({
-            ok: false,
-            err: {
-                message: 'No se ha seleccionado ningun archivo.'
-            }
-        });
-    };
+        return res.status(400)
+            .json({
+                ok: false,
+                err: {
+                    message: 'No se ha seleccionado ningún archivo'
+                }
+            });
+    }
 
-    //Validar tipos 
-    let tiposValidos = ['usuarios', 'productos'];
-
+    // Valida tipo
+    let tiposValidos = ['productos', 'usuarios'];
     if (tiposValidos.indexOf(tipo) < 0) {
         return res.status(400).json({
             ok: false,
             err: {
-                message: 'Los tipos permitidos son ' + tiposValidos.join(', '),
-                tipo
+                message: 'Los tipos permitidas son ' + tiposValidos.join(', ')
             }
         })
-
     }
-    //req.files.(archivo) = la variable archivo es la misma que se envia por parametro en postman para cargar el archivo
+
     let archivo = req.files.archivo;
-    //Variable para obtener el nombre y la extension del archivo segmentado 
     let nombreCortado = archivo.name.split('.');
-    //Variable para obtener la ultima posicion del "nombreCortado"
     let extension = nombreCortado[nombreCortado.length - 1];
 
-
-    //Extensiones permitidas
+    // Extensiones permitidas
     let extensionesValidas = ['png', 'jpg', 'gif', 'jpeg'];
 
     if (extensionesValidas.indexOf(extension) < 0) {
@@ -60,35 +53,40 @@ app.put('/upload/:tipo/:id', (req, res) => {
                 message: 'Las extensiones permitidas son ' + extensionesValidas.join(', '),
                 ext: extension
             }
-        });
-    };
+        })
+    }
 
-    //Cambiar nombre del archivo 
-    let nombreArchivo = `${id}-${ new Date().getMilliseconds()}.${extension}`;
+    // Cambiar nombre al archivo
+    // 183912kuasidauso-123.jpg
+    let nombreArchivo = `${ id }-${ new Date().getMilliseconds()  }.${ extension }`;
 
-    //Ruta donde se subiran los archivos enviados desde postman
-    archivo.mv(`uploads/${tipo}/${nombreArchivo}`, (err) => {
 
-        if (err) {
+    archivo.mv(`uploads/${ tipo }/${ nombreArchivo }`, (err) => {
+
+        if (err)
             return res.status(500).json({
                 ok: false,
                 err
             });
-        };
 
-        //Actualizar imagen en usuario 
+        // Aqui, imagen cargada
         if (tipo === 'usuarios') {
             imagenUsuario(id, res, nombreArchivo);
         } else {
             imagenProducto(id, res, nombreArchivo);
         }
+
     });
+
 });
 
 function imagenUsuario(id, res, nombreArchivo) {
+
     Usuario.findById(id, (err, usuarioDB) => {
+
         if (err) {
-            borrarArchivo(nombreArchivo, 'usuarios');
+            borraArchivo(nombreArchivo, 'usuarios');
+
             return res.status(500).json({
                 ok: false,
                 err
@@ -96,17 +94,18 @@ function imagenUsuario(id, res, nombreArchivo) {
         }
 
         if (!usuarioDB) {
-            borrarArchivo(nombreArchivo, 'usuarios');
+
+            borraArchivo(nombreArchivo, 'usuarios');
+
             return res.status(400).json({
                 ok: false,
                 err: {
-                    message: 'Usuario no existe'
+                    message: 'Usuaro no existe'
                 }
             });
-        };
+        }
 
-        //Borrar imagen del usuario 
-        borrarArchivo(usuarioDB.img, 'usuarios');
+        borraArchivo(usuarioDB.img, 'usuarios')
 
         usuarioDB.img = nombreArchivo;
 
@@ -117,15 +116,24 @@ function imagenUsuario(id, res, nombreArchivo) {
                 usuario: usuarioGuardado,
                 img: nombreArchivo
             });
+
         });
 
+
     });
-};
+
+
+}
+
+
 
 function imagenProducto(id, res, nombreArchivo) {
+
     Producto.findById(id, (err, productoDB) => {
+
         if (err) {
-            borrarArchivo(nombreArchivo, 'productos');
+            borraArchivo(nombreArchivo, 'productos');
+
             return res.status(500).json({
                 ok: false,
                 err
@@ -133,17 +141,18 @@ function imagenProducto(id, res, nombreArchivo) {
         }
 
         if (!productoDB) {
-            borrarArchivo(nombreArchivo, 'productos');
+
+            borraArchivo(nombreArchivo, 'productos');
+
             return res.status(400).json({
                 ok: false,
                 err: {
-                    message: 'Producto no existe'
+                    message: 'Usuaro no existe'
                 }
             });
-        };
+        }
 
-        //Borrar imagen del usuario 
-        borrarArchivo(productoDB.img, 'productos');
+        borraArchivo(productoDB.img, 'productos')
 
         productoDB.img = nombreArchivo;
 
@@ -154,19 +163,25 @@ function imagenProducto(id, res, nombreArchivo) {
                 producto: productoGuardado,
                 img: nombreArchivo
             });
+
         });
 
+
     });
-};
 
-function borrarArchivo(nombreImagen, tipo) {
-    //verificar que la ruta de la imagen exista 
-    //cada argumento del resolve son segmentos del path que se quiere construir
-    let pathImagen = path.resolve(__dirname, `../../uploads/${tipo}/${nombreImagen}`);
 
-    //validar si ese path exista
+}
+
+
+
+function borraArchivo(nombreImagen, tipo) {
+
+    let pathImagen = path.resolve(__dirname, `../../uploads/${ tipo }/${ nombreImagen }`);
     if (fs.existsSync(pathImagen)) {
         fs.unlinkSync(pathImagen);
     }
+
+
 }
+
 module.exports = app;
